a5c55a5f75c0002ccf8bef8c9bb00d1e12b1d0f1
Prabhjot Singh Assi <prabhjotassi16@gmail.com>
2024-04-28 23:10:18 +0530

    Buttons added.

--- Interface/gui_base.py
+++ Interface/gui_base.py
@@ -26,9 +26,46 @@ class GUI:
         self.image_1 = self.load_image("image_1.png", 750.0, 53.0)
         self.image_2 = self.load_image("image_2.png", 759.0, 447.0)
         self.image_3 = self.load_image("image_3.png", 55.0, 390.0)
+        
         self.image_4 = self.load_image("image_4.png", 50.0, 459.0)
+        self.button_4 = Button(
+            self.window,
+            image=self.image_4,
+            bg="#141416",
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.on_button_4_click,
+        )
+        self.button_4.place(x=20.0, y=420.0)
+        
         self.image_5 = self.load_image("image_5.png", 50.0, 260.0)
+        self.button_5 = Button(
+            self.window,
+            image=self.image_5,
+            bg="#141416",
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.on_button_5_click,
+        )
+        self.button_5.place(x=20.0, y=222.0)
+        
         self.image_6 = self.load_image("image_6.png", 50.0, 363.0)
+        self.button_6 = Button(
+            self.window,
+            image=self.image_6,
+            bg="#141416",
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.on_button_6_click,
+        )
+        self.button_6.place(x=20.0, y=320)
+        
+    def on_button_4_click(self):
+        print("Button 4 clicked!")    
+    def on_button_5_click(self):
+        print("Button 5 clicked!")    
+    def on_button_6_click(self):
+        print("Button 6 clicked!")    
 
     @staticmethod
     def relative_to_assets(path: str) -> Path:
